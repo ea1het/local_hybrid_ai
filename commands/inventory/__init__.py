67416f25e9c883265f0ef7871e5395e94161ac15
Jonathan González <135473821+ea1het@users.noreply.github.com>
2026-09-17 14:50:13 +0200

    chore: mark inventory package self-contained

--- commands/inventory/__init__.py
+++ commands/inventory/__init__.py
@@ -1,6 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0.
-"""Source inventory command package."""
+"""Self-contained source inventory command package."""
 from . import api
 from .api import SCHEMA_VERSION,_error,json_payload,cli_text,main
 __all__=["api","SCHEMA_VERSION","_error","json_payload","cli_text","main"]
