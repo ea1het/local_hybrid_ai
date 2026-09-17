fadfa7f94cd5b564c0077da59612afbeedf09ee0
Jonathan González <135473821+ea1het@users.noreply.github.com>
2026-09-17 21:02:53 +0200

    test: document upgrade package boundary module

--- commands/upgrade/tests/test_package_boundary.py
+++ commands/upgrade/tests/test_package_boundary.py
@@ -1,5 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0.
+"""Package-boundary tests for the upgrade command package."""
 from pathlib import Path
 import unittest
 
