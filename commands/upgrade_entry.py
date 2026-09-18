d505583af84bf5de95f92da398118accc05c7994
Jonathan González <ea1het@ea1het.com>
2026-09-18 11:24:15 +0200

    structural refactor

--- commands/upgrade_entry.py
+++
@@ -1,4 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0.
-"""Compatibility shim; Upgrade is owned by commands.upgrade."""
-from commands.upgrade.api import *
--- commands/upgrade_registry.py
+++
@@ -1,4 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0.
-"""Compatibility shim for the package-owned Upgrade registry implementation."""
-from commands.upgrade.registry import *
